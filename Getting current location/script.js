b6cc8f26cf6d3fc79adc35f46025e81402dde0a2
MuhammadShariq05 <shariqwrk12023@gmail.com>
2024-02-29 15:57:10 +0530

    calling apis

--- Getting current location/script.js
+++ Getting current location/script.js
@@ -1,12 +1,21 @@
-const locat = document.getElementById("clickbutton");
+const locate = document.getElementById("clickbutton");
+const display = document.getElementById("display");
 
-function gotLocation(position){
-
+async function getData(lat, long){
+  const promise = await fetch(`http://api.weatherapi.com/v1/current.json?key=690648a6f9e24fe981f152713242702&q=${lat},${long}&aqi=yes`);
+  return await promise.json();
 }
+
+
+async function gotLocation(position){
+  const results = await getData(position.coords.latitude, position.coords.longitude);
+  console.log(results);
+  display.innerText = `${results.location.name}, ${results.location.region} - ${results.location.country}`
+}
+
 function failLocation(){
   console.log("Unable to get location");
 }
-
-locat.addEventListener("click", async () =>{
-  navigator.geolocation.getCurrentPosition(gotLocation, failLocation);
+locate.addEventListener("click", async () =>{
+  navigator.geolocation.getCurrentPosition(gotLocation,failLocation);
 });
